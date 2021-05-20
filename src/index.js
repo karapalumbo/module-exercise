559cae842d44bc59cc17ecbba36696f01b259b66
Kara <palumbokara@yahoo.com>
2021-05-20 15:19:14 -0600

    adding module exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {choice, remove} from './helpers'
+import foods from './foods'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let fruit = choice(foods);
+
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remaining = remove(foods, fruit);
+
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
